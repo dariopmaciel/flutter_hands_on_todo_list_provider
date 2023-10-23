89565c32eca450eceb4db4f5238bf5a98a52218c
dariopmaciel <dariodepaulamaciel@hotmail.com>
2023-10-23 17:15:29 -0300

    Slidable feito

--- lib/app/modules/home/widgets/home_tasks.dart
+++ lib/app/modules/home/widgets/home_tasks.dart
@@ -4,6 +4,7 @@ import 'package:flutter_hands_on_todo_list_provider/app/models/task_filter_enum.
 import 'package:flutter_hands_on_todo_list_provider/app/models/task_model.dart';
 import 'package:flutter_hands_on_todo_list_provider/app/modules/home/home_controller.dart';
 import 'package:flutter_hands_on_todo_list_provider/app/modules/home/widgets/task.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 
 class HomeTasks extends StatelessWidget {
@@ -27,23 +28,50 @@ class HomeTasks extends StatelessWidget {
               );
             },
           ),
+          //---------------------------------------------------------------
+          // Column(
+          //   children:
+          // context
+          //       .select<HomeController, List<TaskModel>>(
+          //           (controller) => controller.filteredTasks)
+          //       .map(
+          //         (task) => Task(
+          //           model: task,
+          //         ),
+          //       )
+          //       .toList(),
+          // ),
+          //---------------------------------------------------------------
           Column(
             children: context
                 .select<HomeController, List<TaskModel>>(
                     (controller) => controller.filteredTasks)
-                .map((task) => Task(model: task,))
+                .map(
+                  (task) => Slidable(
+                    endActionPane: ActionPane(
+                      extentRatio: 0.30,
+                      motion: const StretchMotion(),
+                      children: [
+                        SlidableAction(
+                          onPressed: (context) {
+                            // onDelete(task);
+                          },
+                          icon: Icons.delete,
+                          borderRadius: BorderRadius.circular(20),
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          label: "Deletar",
+                        ),
+                      ],
+                    ),
+                    child: Task(model: task),
+                  ),
+                )
                 .toList(),
           )
+          //---------------------------------------------------------------
         ],
       ),
     );
   }
-
-  // @override
-  // List<Object?> get props => [];
-
-  // @override
-  // String toString() {
-  //   return 'HomeTasks{}';
-  // }
 }
